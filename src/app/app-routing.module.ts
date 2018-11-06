e45aef6750730a3a525f7cdfae8e09f67f661060
Francisco Javier Diaz Martin <fjdiaz@stratio.com>
2018-11-06 10:33:27 +0100

    router 5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from './ui/containers/layout/layout.component';
 
 const routes: Routes = [{
-  path: 'comicstore',
+  path: '',
   component: LayoutComponent,
   children: [{
     path: '',
